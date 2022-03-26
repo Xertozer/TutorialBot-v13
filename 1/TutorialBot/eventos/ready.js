f5bae46d3bf07cbb9f049fc40380ca1bd2ac9d20
Xertozer <89105475+Xertozer@users.noreply.github.com>
2022-03-25 19:08:09 -0500

    Update ready.js

--- 1/TutorialBot/eventos/ready.js
+++ 1/TutorialBot/eventos/ready.js
@@ -7,4 +7,5 @@ module.exports = (client) => {
 async function consola(client) {
     console.log(chalk.blue("Bot iniciado como: ") + chalk.green(client.user.tag))
     console.log(chalk.blue("Fecha de inicio: ") + chalk.green(new Date().toLocaleString("es-es")))
+    console.log(chalk.blue("Hecho por: ") + chalk.green("Xertozer(https://www.youtube.com/channel/UCiwb_-YNs5o8Uag8c2QGv9g)"))
 }

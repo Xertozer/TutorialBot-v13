48858dff42b22d60a9e18d2ae60a25cb55209c49
Xertozer <89105475+Xertozer@users.noreply.github.com>
2022-03-25 21:24:14 -0500

    Update bot.js

--- 1/TutorialBot/bot.js
+++ 1/TutorialBot/bot.js
@@ -1,18 +1,21 @@
-const config = require("./config/config.json")
-const Discord = require("discord.js")
+const config = require("./config/config.json") // definición de las configuraciones del bot
+const Discord = require("discord.js") // definicion del NPM usado
 const client = new Discord.Client({
-    intents: Object.keys(Discord.Intents.FLAGS)
-})
+    intents: Object.keys(Discord.Intents.FLAGS) // definicion del client/bot
+})                                              // con sus intents habilitados para los eventos
 
 const eventos = {
     messageCreate: require("./eventos/messageCreate"),
     ready: require("./eventos/ready")
-}
+} // definicion de los archivos a importar
 
+
+/* definicion de los eventos, con las propiedades importadas automáticamente */
 client.on("messageCreate", eventos.messageCreate)
 client.on("ready", eventos.ready)
 client.login(config.token)
 
+/* definición del handler de erorres, para que el bot no se apague por un bug */
 process.on("unhandledRejection", error => {
     console.log("Hubo un error:", error)
 })

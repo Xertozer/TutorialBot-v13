be2b6883f699691261d4f4dc4b201e242a6c466b
Xertozer <89105475+Xertozer@users.noreply.github.com>
2022-03-25 21:27:12 -0500

    Update ready.js

--- 1/TutorialBot/eventos/ready.js
+++ 1/TutorialBot/eventos/ready.js
@@ -1,10 +1,11 @@
-const chalk = require("chalk")
+const chalk = require("chalk") // definicion del NPM chalk para los colores
 
-module.exports = (client) => {
-    consola(client)
+module.exports = (client) => { // exportar la funcion que importa el eventos.ready en bot.js
+    consola(client) // ejecutar la funcion consola
 }
 
 async function consola(client) {
+    // mostrar por consola los datos
     console.log(chalk.blue("Bot iniciado como: ") + chalk.green(client.user.tag))
     console.log(chalk.blue("Fecha de inicio: ") + chalk.green(new Date().toLocaleString("es-es")))
     console.log(chalk.blue("Hecho por: ") + chalk.green("Xertozer(https://www.youtube.com/channel/UCiwb_-YNs5o8Uag8c2QGv9g)"))

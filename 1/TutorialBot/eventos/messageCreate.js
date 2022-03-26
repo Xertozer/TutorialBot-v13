d2ba10367ca50dba4bcaea225a67fb22d168914e
Xertozer <89105475+Xertozer@users.noreply.github.com>
2022-03-25 21:28:59 -0500

    Update messageCreate.js

--- 1/TutorialBot/eventos/messageCreate.js
+++ 1/TutorialBot/eventos/messageCreate.js
@@ -1,20 +1,20 @@
-const Discord = require("discord.js")
-const config = require("../config/config.json")
+const Discord = require("discord.js") // definicion del NPM discord.js
+const config = require("../config/config.json") // definicion del config del bot
 
-module.exports = (message) => {
-    comandos(message)
+module.exports = (message) => { // exportar la funcion eventos.messageCreate del bot.js
+    comandos(message) // ejecutar la funcion comandos
 }
 
-async function comandos(message) {
-    if (message.content.startsWith(`${config.prefix}ping`)) {
-        message.channel.send({
+async function comandos(message) { // definir la funcion comandos
+    if (message.content.startsWith(`${config.prefix}ping`)) { // si el contenido del mensaje empieza con prefix + ping
+        message.channel.send({ // enviar un mensaje respondiendo
             content: "He recibido el ping, regreso un pong!",
             reply: { messageReference: message.id, failIfNoExists: false }
         }).catch(() => { })
     }
 
-    else if (message.content.startsWith(`${config.prefix}embed`)) {
-        const embed = new Discord.MessageEmbed()
+    else if (message.content.startsWith(`${config.prefix}embed`)) { // si empieza con prefix + embed
+        const embed = new Discord.MessageEmbed() // definir un embed con todas las variables
             .setTitle("Título del embed")
             .setDescription("Descripción del embed")
             .setColor("#fca103")
@@ -30,13 +30,14 @@ async function comandos(message) {
             .setTimestamp()
             .setURL("https://discord.com")
 
-        message.channel.send({
+        message.channel.send({ // enviar el embed
             embeds: [embed],
             reply: { messageReference: message.id, failIfNoExists: false }
         }).catch(() => { })
     }
 
-    else if (message.content.startsWith(config.prefix)) return message.channel.send({
+    // si no existe ese comando, enviar un mensaje explicandolo
+    else if (message.content.startsWith(config.prefix)) return message.channel.send(
         content: "No he encontrado ese comando",
         reply: { messageReference: message.id, failIfNoExists: false }
     }).catch(() => { })
